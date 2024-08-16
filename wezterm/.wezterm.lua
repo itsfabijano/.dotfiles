0c02b4de54a759e8d39fe5e0a83dbb2704f7c85d
itsfabijano <fabian.ehringer@gmx.net>
2024-08-16 08:08:23 +0200

    wezterm alt arrow

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -29,5 +29,20 @@ config.font = wezterm.font('JetBrains Mono', { weight = 'Medium' })
 config.font_size = 12.0
 config.line_height = 1.05
 
+config.keys = {
+    -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+    {
+        key = "LeftArrow",
+        mods = "OPT",
+        action = wezterm.action { SendString = "\x1bb" }
+    },
+    -- Make Option-Right equivalent to Alt-f; forward-word
+    {
+        key = "RightArrow",
+        mods = "OPT",
+        action = wezterm.action { SendString = "\x1bf" }
+    },
+}
+
 -- and finally, return the configuration to wezterm
 return config
