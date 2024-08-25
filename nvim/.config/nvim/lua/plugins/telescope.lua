6058967cab8b4862e063ccdf82891d93ad592d14
itsfabijano <fabian.ehringer@gmx.net>
2024-08-25 23:12:22 +0200

    some other things

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -35,7 +35,7 @@ return {
 
         local builtin = require("telescope.builtin")
         vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-        vim.keymap.set("n", "<leader>p", builtin.git_files, {})
+        vim.keymap.set("n", "<leader>pg", builtin.git_files, {})
 
         vim.api.nvim_create_user_command('CustomFindFiles', function()
             pcall(function()
