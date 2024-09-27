48cc58fd0c89fdcd6e4c8137464b097db7b0f663
itsfabijano <fabian.ehringer@gmx.net>
2024-09-27 14:52:24 +0200

    background color of lsp autosuggest popup

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -7,6 +7,12 @@ function ColorMyPencils(color)
 	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#2e323b" })
 	-- vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+
+	-- Set the background color of the popup menu without changing the text color
+	vim.cmd([[highlight Pmenu guibg=#2e323b]])
+
+	-- Set the background color of the selected item in the popup menu
+	vim.cmd([[highlight PmenuSel guibg=#61AFEF]])
 end
 
 return {
